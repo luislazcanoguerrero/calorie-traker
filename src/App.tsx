49a1400622332034cd60f8fae7a6d0dacd155763
Luis Lazcano Guerrero    <llazcano@dgac.gob.cl>
2025-07-31 05:48:58 -0400

    avance

--- src/App.tsx
+++ src/App.tsx
@@ -5,14 +5,14 @@ function App() {
     <>
       <header className="bg-lime-600 py-3">
         <div className="max-w-4xl mx-auto flex justify-between">
-            <h1 className="text-center text-lg font-bold text-white uppercase">
+            <h1 className="text-center text-4xl font-bold text-white uppercase">
               Contador de Calorias
             </h1>
         </div>
       </header>
       <section className="bg-lime-400 py-20 px-5">
         <div className="max-w-4xl mx-auto">
-          <Form />
+            <Form />
         </div>
       </section>
     </>
--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,8 +1,31 @@
+import { categories } from "../data/categorias"
+console.log(categories)
 
 export default function Form() {
   return (
     <div>
-        <p>Form</p>
+
+      <form className="space-y-5 bg-white shadow p-10 rounded-lg">
+
+        <div className="grid grid-cols-1 gap-3">
+          <label htmlFor="categoriaId" className="font-bold">Categorias</label>
+          <select name="" id="categoriaId" className="border border-slate-300 p-2 rounded-lg w-full bg-white" >
+            {
+              categories.map(items => (
+                <option value="" key={items.id} >
+                  {items.name}
+                </option>
+              ))
+
+            }
+
+          </select>
+
+        </div>
+
+      </form>
+
+
     </div>
   )
 }
