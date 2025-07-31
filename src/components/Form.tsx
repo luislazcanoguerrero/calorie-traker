7310e5a027ec7cf102ec8ade9b91ac6c0bebeee6
Luis Lazcano Guerrero <llazcano@dgac.gob.cl>
2025-07-31 11:25:25 -0400

    Creacion del formulario

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -6,7 +6,7 @@ export default function Form() {
     <div>
 
       <form className="space-y-5 bg-white shadow p-10 rounded-lg">
-
+        {/* CAMPO DE LAS CATEGOTIAS  */}
         <div className="grid grid-cols-1 gap-3">
           <label htmlFor="categoriaId" className="font-bold">Categorias</label>
           <select name="" id="categoriaId" className="border border-slate-300 p-2 rounded-lg w-full bg-white" >
@@ -16,15 +16,36 @@ export default function Form() {
                   {items.name}
                 </option>
               ))
-
             }
-
           </select>
-
         </div>
 
-      </form>
+        {/* CAMPO DE LAS ACTIVODADES  */}
+        <div className="grid grid-cols-1 gap-3">
+          <label htmlFor="actividadId" className="font-bold">Actividad</label>
+          <input type="text"
+            id="actividadId"
+            className="border border-slate-300 rounded-lg p-2" 
+            placeholder="Ej. comida, ensaladas, jugo de naranjas, ejercicios, pesas, etc.."
+          />
+        </div>
 
+        {/* CAMPO DE LAS CALORIAS */}
+        <div className="grid grid-cols-1 gap-3">
+          <label htmlFor="caloriasId" className="font-bold">Calorias</label>
+          <input type="number"
+            id="caloriasId"
+            className="border border-slate-300 rounded-lg p-2" 
+            placeholder="Ej. 200 o 500."
+          />
+        </div>
+         {/* BOTON DE SUBMIT */}
+         <input 
+              type="submit" 
+              className="bg-gray-600  hover:bg-gray-900 w-full p-2 text-white font-bold uppercase cursor-pointer "
+              value={'Guardar Comida o Ejercicio'}
+         />
+      </form>
 
     </div>
   )
